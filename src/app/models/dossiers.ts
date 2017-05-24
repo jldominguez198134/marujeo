0127d3f735467732349857c1af0b18d07fbe9de0
gti <jose@youthinkglobal.com>
2017-05-24 22:26:07 +0200

    Nueva prueba subida

---
+++ src/app/models/dossiers.ts
@@ -0,0 +1,9 @@
+// Modelo de entidades
+
+export class Dossiers {
+  constructor(
+    public id: number,
+    public dossier: string,
+    public fecha: string
+  ){}
+}
--- src/app/modules/dossiers/dossiers/dossiers.component.ts
+++ src/app/modules/dossiers/dossiers/dossiers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {DossiersService} from "../dossiers.service";
+import {Dossiers} from "../../../models/dossiers";
 
 @Component({
   selector: 'app-dossiers',
@@ -8,10 +9,13 @@ import {DossiersService} from "../dossiers.service";
 })
 export class DossiersComponent implements OnInit {
 
-  arrayDossiers: Array<Object>;
+  arrayDossiers: Array<Object>; // Array inicial que guarda datos de la API
+  newArrDossiers: Array<any>; // Array intermedio que va a guardar instancias del modelo con los datos que necesitamos
+  arrayFinal: Array<Object> = []; // Array final que es el que pintará los datos definitivos
 
-  itemsSize: number = 5;
-  p: number = 1;
+  itemsSize: number = 5; // Empezamos con 5 filas
+  p: number = 1; // Empieza en la página 1
+  id: number = 0;
 
 
   constructor(private dossiersService: DossiersService) {
@@ -20,6 +24,30 @@ export class DossiersComponent implements OnInit {
     this.dossiersService.getDossiers()
       .subscribe((dossiers) => {
         this.arrayDossiers = dossiers['data']['results'];
+
+        // Recorremos el array inicial
+        for (let d of this.arrayDossiers) {
+
+          // Le sumamos uno a "p", ya que este es un dato que la api no nos trae como queremos
+          this.id = this.id + 1;
+
+          // Igualamos array intermedio a una serie de instancias del modelo (uno de los parámetros que tendriamos que modificar si añadimos datos a la lista)
+          this.newArrDossiers = [
+            new Dossiers(this.id, d['name'], d['modified'])
+          ];
+
+          // Accedemos a los datos finales
+          this.newArrDossiers = this.newArrDossiers[0];
+
+          // Añadimos al array final objetos cons los datos exactos de cada fila
+          this.arrayFinal.push(
+            {
+              id: this.newArrDossiers['id'], // Número de dossier
+              dossier: this.newArrDossiers['dossier'], // Nombre del dossier
+              fecha: this.newArrDossiers['fecha'] // Fecha de introducción del dossier
+            }
+          );
+        }
       });
   }
 
